9f7b86b5637174be25c260870fdced62d4e0d655
Luis Fernando Soares <luis_ssoares016@hotmail.com>
2023-03-02 20:04:58 -0300

    Refatoração de código para TS e adição de comentários

---
+++ blockchain/models.py
@@ -0,0 +1,18 @@
+from django.db import models
+
+# Create your models here.
+
+class Blocks(models.Model):
+    index = models.PositiveBigIntegerField(blank=False, null=False)
+    transaction = models.JSONField(blank=False, null=False)
+    created_at = models.DateTimeField(auto_now_add=True)
+    previous_hash = models.CharField(blank=False, null=False, max_length=64)
+    nonce = models.PositiveIntegerField(blank=False, null=False)
+    hash = models.CharField(blank=False, null=False, max_length=64)
+    
+class PendingTransactions(models.Model):
+    transaction = models.JSONField(blank=False, null=False)
+    created_at = models.DateTimeField(auto_now_add=True)
+
+class Peers(models.Model):
+    ip_address = models.CharField(blank=False, null=False, max_length=30)
