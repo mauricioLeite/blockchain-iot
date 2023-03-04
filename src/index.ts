ed629ac0ea30972158df30ca54b5b2fca2012f4a
mauricioLeite <mauricioleite.fe@gmail.com>
2023-03-04 17:06:04 -0300

    feat: create DatabaseConnector Instance to test class

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,10 @@
+import "module-alias/register";
+
+import { DatabaseConnector } from "@database";
 
 const main = async () => {
-    
-};
+    const connection = await new DatabaseConnector().createConnection();
+    if (connection) return;
+}
 
 main()
