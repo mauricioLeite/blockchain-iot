59aa6e72edefcb0499b648049a5505a7a58a53c9
mauricioLeite <mauricioleite.fe@gmail.com>
2023-03-18 19:21:28 -0300

    refactor: using getConnection to return DataSource to caller

--- src/database/DatabaseConnector.ts
+++ src/database/DatabaseConnector.ts
@@ -4,13 +4,22 @@ import ormConfig  from "./ormconfig"
 
 
 export class DatabaseConnector {
+    #connection: DataSource
     
-    async createConnection() {
+    async getConnection() : Promise<DataSource> {
+        if ( !this.#connection ) await this.#createConnection();
+        return this.#connection;
+    }
+    
+    async #createConnection() {
         const connection = new DataSource(ormConfig)
 
-        return connection.initialize().catch(err => {
+        await connection.initialize().catch(err => {
             console.log(err);
             return null;
         });
+        
+        this.#connection = connection;
     }
+
 }
