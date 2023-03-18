7d1f78411dc943a59e15beee6c6d14738509b0c1
mauricioLeite <mauricioleite.fe@gmail.com>
2023-03-18 19:19:34 -0300

    refactor: using path on entities and migrations options

--- src/database/ormconfig.ts
+++ src/database/ormconfig.ts
@@ -4,9 +4,9 @@ import { DataSourceOptions } from "typeorm";
 const DSOptions: DataSourceOptions = {
     type: "better-sqlite3",
     database: "data/localbase.sqlite",
-    entities: [path.join("**", "entities", "*.entity.{js,ts}")],
+    entities: [path.join(__dirname, "**", "entities", "*.entity.{js,ts}")],
     migrationsTableName: "migrations",
-    migrations: ["dist/database/migrations/*.js"]
+    migrations: [path.join(__dirname, "**", "migrations", "*.{js,ts}")]
 }
 
 export default DSOptions
