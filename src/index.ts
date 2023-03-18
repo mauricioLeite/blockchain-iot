e3aefc740000588be1f58ea9094641954b8f1d13
mauricioLeite <mauricioleite.fe@gmail.com>
2023-03-18 19:18:37 -0300

    refactor: made main only return

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,7 @@
 import "module-alias/register";
 
-import { DatabaseConnector } from "@database";
-
 const main = async () => {
-    const connection = await new DatabaseConnector().createConnection();
-    if (connection) return;
+    return;
 }
 
 main()
