a3c514e522d7f5da4f9fe08fdbfb5843914a8f51
mauricioLeite <mauricioleite.fe@gmail.com>
2023-06-30 11:23:35 -0300

    test: create Transactions class test

---
+++ src/services/transactions.unit.test.ts
@@ -0,0 +1,25 @@
+import { Transactions  } from "./transactions";
+import { DatabaseResourceFactory } from "@database";
+
+import { generateRandomMacAddress } from "@utils/generate";
+
+describe("Transactions Service", () => {
+
+    describe("#init", () => {
+        const instance: Transactions = new Transactions(new DatabaseResourceFactory());
+        
+        it("should create a transaction", async () => {
+            const transaction: object = { macAddress: generateRandomMacAddress() };
+            const result = await instance.create(transaction);
+            expect(result).toHaveProperty('status');
+            expect(result.status).toBe(201);
+        })
+
+        it("should list the pending transactions", async () => {
+            const result = await instance.pending();
+            expect(result).toHaveProperty('status');
+            expect(result.status).toBe(200);
+        })
+
+    })
+})
