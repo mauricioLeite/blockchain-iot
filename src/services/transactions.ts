b881474a482b1468ec3fd2dc42a2687db5303b47
mauricioLeite <mauricioleite.fe@gmail.com>
2023-06-30 10:21:23 -0300

    refactor: adjust class methods to integrate DB

--- src/services/transactions.ts
+++ src/services/transactions.ts
@@ -1,26 +1,29 @@
+import { DatabaseResourceFactory } from "@database";
+
 export class Transactions {
-    storage: any
+    #storage: DatabaseResourceFactory
 
-    constructor(_storage: any) {
-        this.storage = _storage
+    constructor(storage: any) {
+        this.#storage = storage
     }
 
-    create(payload: any) {
-        const inserted = this.storage
-            .createPendingTransactionsModel()
-            .insert({ transaction: payload })
-        if (inserted)
-            return { message: "Transaction requested!", status: 201 }
-
-        return { message: "Internal Error", status: 500 }
+    async create (payload: any) {
+        const transactionModel = await this.#storage.createPendingTransactionsResource();
+        const inserted = await transactionModel.create({ transaction: payload });
+        
+        if (inserted)   return { message: "Transaction requested!", status: 201 };
+        return { message: "Internal Error", status: 500 };
     }
 
-    pending() {
-        const pendingTx = this.storage.createPendingTransactionsModel().getAll()
+    async pending() {
+        const transactionModel = await this.#storage.createPendingTransactionsResource();
+        const pendingTransaction = await transactionModel.getAll();
+
+        if (!pendingTransaction)   return { message: "Internal Error", status: 500 };
         return {
-            unconfirmedTransactions: pendingTx,
-            length: pendingTx.length,
+            unconfirmedTransactions: pendingTransaction,
+            length: pendingTransaction.length,
             status: 200,
-        }
+        };
     }
 }
