e8a11a0a2bffc3a81b658d1618f624d53fa147ff
mauricioLeite <mauricioleite.fe@gmail.com>
2023-06-30 10:20:19 -0300

    feat: create index file to export services classes

---
+++ src/services/index.ts
@@ -0,0 +1,4 @@
+export { Mine as MineService } from "./mine";
+export { Nodes as NodesService } from "./nodes";
+export { Registry as RegistryService } from "./registry";
+export { Transactions as TransactionService } from "./transactions";
