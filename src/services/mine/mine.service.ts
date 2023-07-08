3a241b5cbd3f7e13d6d88ee570b9996277e1783e
mauricioLeite <mauricioleite.fe@gmail.com>
2023-07-08 14:32:31 -0300

    feat: adjust mining process to synchronize new block

--- src/services/mine/mine.service.ts
+++ src/services/mine/mine.service.ts
@@ -20,17 +20,21 @@ export class Mine {
         const minedBlockId = await blockchain.mine(transaction);
         if (!minedBlockId)  return { response: { message: "Error on mining process." }, status: 500 };
         
-        const chain = await blockchain.chain();
-        const chainLength = chain.length;
+        let chain = await blockchain.chain();
+        const originalLength = chain.length;
 
-        await this.consensus(chainLength);
+        await this.consensus(originalLength);
         const lastBlock = await blockchain.lastBlock();
-        return { response: {message:"TESTING"}, status: 404 };
-        if (chainLength === blockchain.chain.length) {
+
+        chain = await blockchain.chain();
+        const afterConsensusLength = chain.length;
+
+        console.log(originalLength, ' ---- ', afterConsensusLength);
+        if (originalLength === afterConsensusLength) {
             await this.announceNewBlock(lastBlock);
 
             const transactionModel = await this.#storage.createPendingTransactionsResource();
-            transactionModel.deleteById(transaction.id);
+            await transactionModel.deleteById(transaction.id);
         }
 
         return {
@@ -40,18 +44,20 @@ export class Mine {
     }
 
     async consensus(chainCurrentLength: number) {
+        console.log("================= CONSENSUS =================");
         const blockchain = await this.#core.createBlockchain();
         const peersModel = await this.#storage.createPeersResource();
         let longestChain = null;
 
         let longestChainLength = chainCurrentLength;
-        const peers = peersModel.getAll();
+        const peers = await peersModel.getAll();
+        console.log("peers --->", peers);
 
         const client = new HTTPRequest('');
         for (const node of peers) {
             client.baseURL = `http://${node.ip_address}`;
             const response = await client.get(`/registry`);
-
+            console.log(node, " - response:" , response);
             const length = response.data.length
             const chain = response.data.chain
             if ( length > longestChainLength && await blockchain.checkChainValidity(chain) ) {
@@ -62,26 +68,26 @@ export class Mine {
         if (longestChain) {
             await blockchain.createChainFromDump(longestChain);
         }
+        console.log("================= CONSENSUS =================");
 		return
     }
 
-    async announceNewBlock(block: Block) {
+    async announceNewBlock(block: any) {
         for (const key of Object.keys(block)) {
-            if (!['index','transaction','nonce','previous_hash', 'hash'].includes(key)) delete block[key as keyof Block];
+            if (!['index','transaction','nonce','previous_hash', 'hash'].includes(key)) delete block[key];
         }
 
         const peersModel = await this.#storage.createPeersResource();
-        const peers = peersModel.getAll();
+        const peers = await peersModel.getAll();
+        
+        if (!peers) return;
 
-        const options: ClientOptions = {
-            method: 'POST',
-            params: { json: JSON.stringify(block) },
-        };
-        const client = new Axios()
+        const client = new HTTPRequest('');
+        const data = { json: JSON.stringify(block) }
         
         for (const node of peers) {
-            options.url = `http://${node.ip_address}/node/sync_block`;
-            await client.request(options); 
+            client.baseURL = `http://${node.ip_address}`
+            const response = await client.post(`/nodes/sync_block`, data);
         }
     }
 }
