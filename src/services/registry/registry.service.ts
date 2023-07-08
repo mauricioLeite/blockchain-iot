ed4e1edf3b2ca5528a6ff4c736eceb2fd089d4b9
mauricioLeite <mauricioleite.fe@gmail.com>
2023-07-08 13:13:45 -0300

    feat: return transactions on registry list

--- src/services/registry/registry.service.ts
+++ src/services/registry/registry.service.ts
@@ -10,11 +10,14 @@ export class Registry {
     async list() {
         const blockchain = new Blockchain(this.#storage);
         const peersInstance = new Peers(this.#storage);
+        const transactionModel = await this.#storage.createPendingTransactionsResource();
+
         const peers = await peersInstance.list();
         const chain = await blockchain.chain();
+        const transactions = await transactionModel.getAll();
 
         return { 
-            response: { chain: chain, peers: peers, length: chain.length},
+            response: { chain, peers, transactions, chain_length: chain.length},
             status: 200 
         }
     }
@@ -25,9 +28,11 @@ export class Registry {
      public async clearLocal() {
         const peersModel = await this.#storage.createPeersResource();
         const blockModel = await this.#storage.createDevicesResource();
+        const transactionModel = await this.#storage.createPendingTransactionsResource();
         
         peersModel.truncate();
         blockModel.truncate();
+        transactionModel.truncate();
 
         return { response: { message: 'Clear complete!' }, status: 200};
     }
