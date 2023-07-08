fb6ebd5f91f56f8c2246a33c6f713fd86658ae63
mauricioLeite <mauricioleite.fe@gmail.com>
2023-07-08 10:16:19 -0300

    feat: create post method on TransactionsController

--- src/services/transactions/transactions.controller.ts
+++ src/services/transactions/transactions.controller.ts
@@ -1,7 +1,9 @@
 import { Request, Response } from "express";
 import { Transactions } from "./transactions.service";
+
 import { DatabaseResourceFactory } from "@database";
 import { DatabaseConnector } from "@database/DatabaseConnector";
+import { Logger } from "@utils";
 
 const storage = new DatabaseResourceFactory(new DatabaseConnector());
 
@@ -9,6 +11,15 @@ export class TransactionsController {
 
     async get(_req: Request, res: Response) {
         const response = await new Transactions(storage).pending();
+        return res.json( response );
+    }
+
+    async post(req: Request, res: Response) {
+        const logger = new Logger('transactions.controller.ts', '/services/transactions');
+        logger.info(req.body);
+        
+        const { response, status} = await new Transactions(storage).create(req.body);
+        res.status(status);
         return res.json( response );
     }
 
