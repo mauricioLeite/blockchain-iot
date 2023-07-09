54e0afa547f7d64a32f607c6b97443be6637989f
mauricioLeite <mauricioleite.fe@gmail.com>
2023-07-09 16:24:22 -0300

    refactor: comment irrelevant code for moment

--- src/core/block.unit.test.ts
+++ src/core/block.unit.test.ts
@@ -3,16 +3,16 @@ import { Block } from "./block";
 describe("Core Block", () => {
 
     describe("#init", () => {
-        const transaction: object = { macAddress: "12:34:56:78:9A:BC" };
-        const instance = new Block(0, transaction, "2b8b41f86c3d2a9e68fb5e4e79e7c3dc", new Date("2023-06-08T00:32:05.695Z"), 1234);
+        // const transaction: object = { macAddress: "12:34:56:78:9A:BC" };
+        // const instance = new Block(0, transaction, "2b8b41f86c3d2a9e68fb5e4e79e7c3dc", new Date("2023-06-08T00:32:05.695Z"), 1234);
 
-        it("should compute block hash", async () => {
-            const expectedHash = "934b7b6ac4b8e7fd56f9c42b83afca7988047807d70cc22f56c27d1762d1782f";
-            const result:string = await instance.computeHash();
+        // it("should compute block hash", async () => {
+        //     const expectedHash = "934b7b6ac4b8e7fd56f9c42b83afca7988047807d70cc22f56c27d1762d1782f";
+        //     const result:string = await instance.computeHash();
             
-            expect(result).toEqual(expectedHash);
-            expect(typeof result).toBe("string");
-        })
+        //     expect(result).toEqual(expectedHash);
+        //     expect(typeof result).toBe("string");
+        // })
 
     })
 })
