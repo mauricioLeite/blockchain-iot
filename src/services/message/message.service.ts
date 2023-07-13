3d5bd8a4f1c6a8a1b33537c1e2c1173e7b384128
mauricioLeite <mauricioleite.fe@gmail.com>
2023-07-13 15:08:59 -0300

    feat: implement logic to check existence of devices on blockchain

--- src/services/message/message.service.ts
+++ src/services/message/message.service.ts
@@ -12,9 +12,21 @@ export class Message {
     }
 
     async processMessage(message: any) {
-        // ! IMPLEMENT LOGIC TO HANDLE MESSAGE AND GENERATE FORWARD TARGET
+        const blockchain = await this.#core.createBlockchain();
+        const actualChain = await blockchain.chain();
+        const validChain = await blockchain.checkChainValidity(actualChain);
+        
+        if (!validChain) return { response: "Chain is tampered!.", target: message.source };
 
-        return { response: message.message  , target: message.toDevice };
+        const blocksModel: any = await this.#storage.createDevicesResource();
+        const sourceCheck = await blocksModel.find({ "uuid": message.source });
+        const targetCheck = await blocksModel.find({ "uuid": message.target });
+        
+        if (!sourceCheck || !targetCheck) {
+            return { response: "Unauthorized action.", target: message.source };
+        }
+        return { response: message.message, target: message.target };
+        
     }
 
 }
